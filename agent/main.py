7d76b298d7e0eb4fd00e9576735ba22a5278f10f
Soda <soda_code@outlook.com>
2025-12-23 14:59:15 +0800

    登录游戏

--- agent/main.py
+++ agent/main.py
@@ -3,9 +3,6 @@ import sys
 from maa.agent.agent_server import AgentServer
 from maa.toolkit import Toolkit
 
-import my_action
-import my_reco
-
 
 def main():
     Toolkit.init_option("./")
@@ -14,7 +11,7 @@ def main():
         print("Usage: python main.py <socket_id>")
         print("socket_id is provided by AgentIdentifier.")
         sys.exit(1)
-        
+
     socket_id = sys.argv[-1]
 
     AgentServer.start_up(socket_id)
---
+++ agent/reco.py
@@ -0,0 +1,3 @@
+from maa.agent.agent_server import AgentServer
+from maa.custom_action import CustomAction
+from maa.context import Context
