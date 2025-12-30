e6ae12651979df7062ca522d57afb55ad276f223
SodaCodeSave <soda_code@outlook.com>
2025-12-30 13:28:50 +0800

    fix: 修复错误的点击确认按钮位置 添加新logo

--- tools/ci/install.py
+++ tools/ci/install.py
@@ -59,6 +59,7 @@ def install_resource():
         working_dir / "assets" / "interface.json",
         install_path,
     )
+    shutil.copy2(working_dir / "assets" / "logo.ico", install_path / "Assets")
 
     with open(install_path / "interface.json", "r", encoding="utf-8") as f:
         interface = json.load(f)
